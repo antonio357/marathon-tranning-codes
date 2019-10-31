09e0ff7e7a3cb1404eb34ea36481cdf5b3ddba29
antonio357 <carlosalbuquerque359@gmail.com>
2019-10-31 15:44:53 -0300

    christmas lights

--- randomTasks/LuzesDeNatal.cpp
+++ randomTasks/LuzesDeNatal.cpp
@@ -3,18 +3,20 @@
 using namespace std;
 
 int main() {
-    int m, n, p, sum, in;
     while (true) {
-        cin >> m;
-        cin >> n;
-        cin >> p;
-        if (m == 0 and n == 0 and p == 0) break;
-        sum = 0;
-        for (int i = 0; i < p; i++) {
-            cin >> in;
-            sum = sum + ((n + 1) - in);
+        int lights_off = 0;
+        int height = 0, lines = 0, num = 0, width = 0;
+
+        cin >> height >> width >> lines;
+        if (height == 0 and width == 0 and lines == 0) break;
+
+        for (int i = 0; i < lines; i++) {
+            cin >> num;
+            num = num % width;
+            if (num == 0) lights_off += 1;
+            else lights_off += width - (num - 1);
         }
-        cout << "Lights: " << sum << endl;
+        cout << "Lights: " << lights_off << endl;
     }
     return 0;
 }
