a8b867aca60a2b5798a927d07fbc6eb5005fdd47
antonio357 <carlosalbuquerque359@gmail.com>
2019-11-23 20:37:23 -0300

    Lista Telefônica Econômica, done {to solve the problem u need to sort the list, this funcionality was missing}

--- RuanContest/EstruturadeDados/ListaTelefonicaEconomica.cpp
+++ RuanContest/EstruturadeDados/ListaTelefonicaEconomica.cpp
@@ -1,41 +1,53 @@
 #include <iostream>
+#include <set>
 
 using namespace std;
 
-int prefixLength(string* string1, string* string2) {
+int prefixLength(const string string1, const string string2) {
     int c = 0;
 
-    for (int i = 0; i < string1->length(); ++i) {
-        if ((*string1)[i] == (*string2)[i]) c++;
+    for (int i = 0; i < string1.length(); ++i) {
+        if ((string1)[i] == (string2)[i]) c++;
         else break;
     }
 
     return c;
 }
 
+unsigned long long int getTotal(set <string>* numbers) {
+    unsigned long long int sum = 0;
+    auto end = numbers->end(); end--;
+
+    for (auto it = numbers->begin(); it != end; ++it) {
+        sum += prefixLength(*it, *(it++)); it--;
+    }
+    return sum;
+}
+
+void printSet(set <string>* numbers) {
+    auto end = numbers->end(); end--;
+
+    cout << "set = ";
+    for (auto it = numbers->begin(); it != end; ++it) {
+        cout << *it << ", ";
+    } cout << endl;
+}
+
 int main() {
     int nCases;
-    unsigned long long int sum;
-    string top_num;
-    string bottom_num;
+    string input;
+    set <string> numbers;
 
     while (cin >> nCases) {
-        sum = 0;
+        numbers.clear();
 
-        cin >> top_num;
-        if (nCases > 1) {
-            cin >> bottom_num;
-
-            sum += prefixLength(&top_num, &bottom_num);
-            for (int i = 0; i < nCases - 2; ++i) {
-                top_num = bottom_num;
-                cin >> bottom_num;
-
-                sum += prefixLength(&top_num, &bottom_num);
-            }
+        for (int i = 0; i < nCases; ++i) {
+            cin >> input;
+            numbers.insert(input);
         }
 
-        cout << sum << endl;
+//        printSet(&numbers);
+        cout << getTotal(&numbers) << endl;
     }
     return 0;
 }
