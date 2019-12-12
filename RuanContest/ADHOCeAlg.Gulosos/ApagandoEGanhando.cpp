506cbcb97b4bc0c953445fa86cc9cf1c1550fbe7
antonio357 <carlosalbuquerque359@gmail.com>
2019-12-12 14:44:26 -0300

    apagando egannhado, fixed bug but the code gets 20% wrong

--- RuanContest/ADHOCeAlg.Gulosos/ApagandoEGanhando.cpp
+++ RuanContest/ADHOCeAlg.Gulosos/ApagandoEGanhando.cpp
@@ -4,10 +4,10 @@
 
 using namespace std;
 
-struct digit {
+typedef struct {
     char num;
     int index;
-};
+} digit;
 
 bool operator<(const digit& d1, const digit& d2) {
    if (d1.num == d2.num) return d1.index < d2.index;
@@ -18,15 +18,33 @@ bool compareDigitByIndex(digit dgt1, digit dgt2) {
     return dgt1.index < dgt2.index;
 }
 
+void printList1(priority_queue <digit>* digits_debug) {
+    cout << "priority_queue = [";
+    while (not digits_debug->empty()) {
+        cout << digits_debug->top().num << ", ";
+        digits_debug->pop();
+    } cout << ']' << endl;
+}
+
+void printArray(digit arr[], int len) {
+    cout << "array = [";
+    for (int i = 0; i < len; ++i) {
+        cout << arr[i].num << ", ";
+    } cout << ']' << endl;
+}
+
 string biggestNumber(string inputNum, int finalLen) {
     string result = "";
 
     // digits sorted by size
     priority_queue <digit> digits;
+//    priority_queue <digit> digits_debug;
     for (int i = 0; i < inputNum.length(); ++i) {
         digit dgt = {inputNum[i], i};
         digits.push(dgt);
+//        digits_debug.push(dgt);
     }
+//    printList1(&digits_debug);
 
     // digits sorted by index
     digit arr[finalLen];
@@ -34,8 +52,11 @@ string biggestNumber(string inputNum, int finalLen) {
         arr[j] = digits.top();
         digits.pop();
     }
-    int n = sizeof(arr) * finalLen/ sizeof(arr[0]);
-    sort(arr, arr+n, compareDigitByIndex);
+//    cout << "before get sorted" << endl;
+//    printArray(arr, finalLen);
+    sort(arr, arr+finalLen, compareDigitByIndex);
+//    cout << "after get sorted" << endl;
+//    printArray(arr, finalLen);
 
     // creating result
     for (int k = 0; k < finalLen; ++k) {
@@ -54,7 +75,8 @@ int main() {
         if (numLen == 0 and delNum == 0) break;
 
         cin >> inputNum;
-        cout << "case = " << biggestNumber(inputNum, numLen - delNum) << endl;
+//        cout << "inputNUm = " << inputNum << endl;
+        cout << biggestNumber(inputNum, numLen - delNum) << endl;
     }
     return 0;
 }
