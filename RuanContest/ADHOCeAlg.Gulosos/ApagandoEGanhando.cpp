0a51f26c7166a0aaeeb633f4fd22b198afa0598d
antonio357 <carlosalbuquerque359@gmail.com>
2019-12-12 16:06:30 -0300

    apagando e ganhando, the solution is to slow for big case tests

--- RuanContest/ADHOCeAlg.Gulosos/ApagandoEGanhando.cpp
+++ RuanContest/ADHOCeAlg.Gulosos/ApagandoEGanhando.cpp
@@ -1,66 +1,28 @@
 #include <iostream>
-#include <queue>
-#include <algorithm>
 
 using namespace std;
 
-typedef struct {
-    char num;
-    int index;
-} digit;
+int getBiggestDigitIndex(string* inputNum, int startIndex, int endIndex) {
+    char biggest = (*inputNum)[startIndex];
+    int index = startIndex;
 
-bool operator<(const digit& d1, const digit& d2) {
-   if (d1.num == d2.num) return d1.index < d2.index;
-   else return d1.num < d2.num;
+    for (int i = startIndex + 1; i < endIndex; ++i) {
+        if ((*inputNum)[i] > biggest) {
+            biggest = (*inputNum)[i];
+            index = i;
+        }
+    }
+    return index;
 }
 
-bool compareDigitByIndex(digit dgt1, digit dgt2) {
-    return dgt1.index < dgt2.index;
-}
-
-void printList1(priority_queue <digit>* digits_debug) {
-    cout << "priority_queue = [";
-    while (not digits_debug->empty()) {
-        cout << digits_debug->top().num << ", ";
-        digits_debug->pop();
-    } cout << ']' << endl;
-}
-
-void printArray(digit arr[], int len) {
-    cout << "array = [";
-    for (int i = 0; i < len; ++i) {
-        cout << arr[i].num << ", ";
-    } cout << ']' << endl;
-}
-
-string biggestNumber(string inputNum, int finalLen) {
+string biggestNumber(string* inputNum, int finalLen) {
     string result = "";
+    int startIndex = 0, index;
 
-    // digits sorted by size
-    priority_queue <digit> digits;
-//    priority_queue <digit> digits_debug;
-    for (int i = 0; i < inputNum.length(); ++i) {
-        digit dgt = {inputNum[i], i};
-        digits.push(dgt);
-//        digits_debug.push(dgt);
-    }
-//    printList1(&digits_debug);
-
-    // digits sorted by index
-    digit arr[finalLen];
-    for (int j = 0; j < finalLen; ++j) {
-        arr[j] = digits.top();
-        digits.pop();
-    }
-//    cout << "before get sorted" << endl;
-//    printArray(arr, finalLen);
-    sort(arr, arr+finalLen, compareDigitByIndex);
-//    cout << "after get sorted" << endl;
-//    printArray(arr, finalLen);
-
-    // creating result
-    for (int k = 0; k < finalLen; ++k) {
-        result += arr[k].num;
+    for (int i = finalLen; i > 0; --i) {
+        index = getBiggestDigitIndex(inputNum, startIndex, inputNum->length() - i + 1);
+        result += (*inputNum)[index];
+        startIndex = index + 1;
     }
 
     return result;
@@ -75,8 +37,7 @@ int main() {
         if (numLen == 0 and delNum == 0) break;
 
         cin >> inputNum;
-//        cout << "inputNUm = " << inputNum << endl;
-        cout << biggestNumber(inputNum, numLen - delNum) << endl;
+        cout << biggestNumber(&inputNum, numLen - delNum) << endl;
     }
     return 0;
 }
