7968ca978f10be3f6650355d331c0b0437dd1156
antonio357 <carlosalbuquerque359@gmail.com>
2019-12-24 10:51:32 -0300

    erasing and writing, done

--- RuanContest/ADHOCeAlg.Gulosos/ApagandoEGanhando.cpp
+++ RuanContest/ADHOCeAlg.Gulosos/ApagandoEGanhando.cpp
@@ -20,42 +20,32 @@ bool operator<=(const digit& d1, const digit& d2){
     return (d1.num == d2.num or d1.num < d2.num);
 }
 
-string biggestNumber(string inputNum, int finalLen, int delNum) {
+void biggestNumber(string inputNum, int finalLen, int delNum) {
     stack <digit> selector;
     int delCounter = 0;
 
     for (int i = 0; i < inputNum.length(); i++) {
         digit dgt = {inputNum[i], i};
-//        cout << "dgt in play = " << dgt.num << endl;
         while (selector.size() > 0 and selector.top() < dgt and delCounter < delNum) {
-//            if (selector.top() == dgt) {
-//                selector.pop();
-//                delCounter++;
-//                cout << "pop first by else" << endl;
-//            }
             selector.pop();
             delCounter++;
-//            cout << "pop by while" << endl;
         }
-        if (selector.size() < finalLen) {
-            selector.push(dgt);
-//            cout << "push by last if" << endl;
-        } else delCounter++;
-//        cout << "end of dgt in play" << endl;
+        if (selector.size() < finalLen) selector.push(dgt);
+        else delCounter++;
     }
 
-    char tempOutputNum[finalLen];
-    for (int i = finalLen - 1; i > -1; i--) {
-        tempOutputNum[i] = selector.top().num;
+    string tempOutputNum;
+    while (not selector.empty()) {
+        if (selector.top().num >= '0' and selector.top().num <= '9') tempOutputNum += selector.top().num;
         selector.pop();
     }
 
     string outputNum;
-    for (int i = 0; i < finalLen; ++i) {
+    for (int i = tempOutputNum.length() - 1; i > -1; --i) {
         outputNum += tempOutputNum[i];
     }
 
-    return outputNum;
+    cout << outputNum << endl;
 }
 
 int main() {
@@ -68,7 +58,7 @@ int main() {
         int finalLen = numLen - delNum;
 
         cin >> inputNum;
-        cout << biggestNumber(inputNum, finalLen, delNum) << endl;
+        biggestNumber(inputNum, finalLen, delNum);
     }
     return 0;
 }
